5a88c9bed647b2c74269780d285589b08db99953
joe342wise <jnyarko342@gmail.com>
2025-05-22 02:05:28 +0000

    Update assembly information and add HTTP request file for GameStore API

--- GameStore.API/obj/Debug/net9.0/GameStore.API.AssemblyInfo.cs
+++ GameStore.API/obj/Debug/net9.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd347023e29b31ffb834c0b7e92068c8ec50bf27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+676fdb22f3b016bc1debb09e0fea16fde360d6da")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
