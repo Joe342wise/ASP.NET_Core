b8ac18c604ba73469c9c456bc393c400db120b40
joe342wise <jnyarko342@gmail.com>
2025-05-26 02:49:35 +0000

    Refactor game deletion to use database context and remove hardcoded game list

--- GameStore.API/Endpoints/GamesEndpoints.cs
+++ GameStore.API/Endpoints/GamesEndpoints.cs
@@ -12,13 +12,13 @@ public static class GamesEndpoints
 {
     const string GetGameEndpointName = "GetGame";
 
-    private static readonly List<GameDto> games = [
-        new (1, "The Witcher 3", "Action RPG", 59.99m, new DateOnly(2015, 5, 19)),
-        new (2, "Dark Souls 3", "Action RPG", 49.99m, new DateOnly(2016, 3, 24)),
-        new (3, "Elden Ring", "Action RPG", 69.99m, new DateOnly(2022, 2, 25)),
-        new (4, "Horizon Zero Dawn", "Action RPG", 39.99m, new DateOnly(2017, 2, 28)),
-        new (5, "The Last of Us Part II", "Action Adventure", 59.99m, new DateOnly(2020, 6, 19)),
-    ];
+    // private static readonly List<GameDto> games = [
+    //     new (1, "The Witcher 3", "Action RPG", 59.99m, new DateOnly(2015, 5, 19)),
+    //     new (2, "Dark Souls 3", "Action RPG", 49.99m, new DateOnly(2016, 3, 24)),
+    //     new (3, "Elden Ring", "Action RPG", 69.99m, new DateOnly(2022, 2, 25)),
+    //     new (4, "Horizon Zero Dawn", "Action RPG", 39.99m, new DateOnly(2017, 2, 28)),
+    //     new (5, "The Last of Us Part II", "Action Adventure", 59.99m, new DateOnly(2020, 6, 19)),
+    // ];
 
     public static RouteGroupBuilder MapGamesEndpoints(this RouteGroupBuilder gamesGroup){
 
@@ -58,10 +58,16 @@ public static class GamesEndpoints
         });
 
         // Delete /games/1
-        gamesGroup.MapDelete("/{id}", (int id) => {
-            games.RemoveAll(game => game.Id == id);
+        gamesGroup.MapDelete("/{id}", (int id, GameStoreContext dbContext) => {
+            Game? game = dbContext.Games.Find(id);
+            if (game == null) return Results.NotFound();
 
-                    return Results.NoContent();
+            dbContext.Games.Remove(game);
+            dbContext.SaveChanges();
+
+            // dbContext.Games.Where(game => game.Id == id).ExecuteDelete();
+
+            return Results.NoContent();
         });
 
         return gamesGroup;
