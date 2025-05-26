929b213f572fe71707e1f4a7eaa7101bb559f3dd
joe342wise <jnyarko342@gmail.com>
2025-05-26 02:40:51 +0000

    Update AssemblyInfo for versioning consistency

--- GameStore.API/obj/Debug/net9.0/GameStore.API.AssemblyInfo.cs
+++ GameStore.API/obj/Debug/net9.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b53eec0d8aef0bd2de9bebfaba152b6be6deb457")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9168c835fe6dcbdc5e7b8408775d97be54da36a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
